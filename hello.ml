59c888c370ecfff614e92db17e724878f4f447af
Marcus Rohrmoser <work@mro.name>
2019-12-11 22:31:02 +0100

    OCaml hello, world 🐫

---
+++ hello.ml
@@ -0,0 +1,12 @@
+(* 
+ * https://caml.inria.fr/pub/docs/u3-ocaml/ocaml-steps.html 
+ *
+ *
+ *)
+print_string "Content-type: text/html; charset=utf-8\n";;
+print_string "\n";;
+print_string "<html>";;
+print_string "<head><title>Hello, Worλd</title></head>";;
+print_string "<body><h1>OCaml, where art thou 🐫!</h1></body>";;
+print_string "</html>";;
+exit;;
